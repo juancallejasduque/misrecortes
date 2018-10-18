a951bf895083f422e44b6e81869fa7ac054ed649
juancallejasduque <juancallejasduque@gmail.com>
2018-10-17 22:57:35 -0500

    Menu initial version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MenuComponent } from './menu/menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
