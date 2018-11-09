bfd0eb6ddeff42709ad7b6e7f857b494e9e6a290
juancallejasduque <juancallejasduque@gmail.com>
2018-11-08 22:26:21 -0500

    Basic functions links to menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 import { MenuComponent } from './menu/menu.component';
+
+/*const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];*/
 
 @NgModule({
   declarations: [
@@ -11,6 +27,10 @@ import { MenuComponent } from './menu/menu.component';
     MenuComponent
   ],
   imports: [
+    /*RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),*/
     BrowserModule
   ],
   providers: [],
